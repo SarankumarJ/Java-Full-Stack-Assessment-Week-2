c37bacc7efdea89a9c55c5afc9bfea700b30bda9
SarankumarJ <sarankumarjeyabalan@gmail.com>
2023-03-19 12:56:19 +0530

    Commit

--- Question-3/Employee_Detail.java
+++ Question-3/Employee_Detail.java
@@ -3,11 +3,11 @@ public class Employee_Detail {
     {
 
         System.out.println("Name " + "    Year of joining " + "                 Address");
-        Employeee e1 =new Employeee("Ram",1994,"                  64C- WallsStreat");
+        Employeee e1 =new Employeee("Robert",1994,"                  64C- WallsStreat");
         e1.Robert();
-        Employeee e2 =new Employeee("Saran",  2000,"                  68D- WallsStreat");
+        Employeee e2 =new Employeee("Sam",  2000,"                  68D- WallsStreat");
         e2.Sam();
-        Employeee e3 =new Employeee("Kumar",1999,"                     26B- WallsStreat");
+        Employeee e3 =new Employeee("John",1999,"                     26B- WallsStreat");
         e1.Robert();
     }
 }
